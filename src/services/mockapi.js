97fe789c1f2f5df62e22b0dcec7a7f6f98c9ba6c
Tate-T <tatjana.tarasovych@gmail.com>
2022-03-31 16:31:00 +0300

    deleted actions

--- src/services/mockapi.js
+++ src/services/mockapi.js
@@ -19,27 +19,3 @@ export const deleteContactApi = (id) => {
         .then(({ data }) => data.id)
         .catch((err) => err);
 };
-
-// export const filterContactsApi = (id) => {
-//     return axios.filter(`/contacts/${id}`)
-//         .then(({ data }) => data.id === id)
-//         .catch((err) => err);
-// };
-
-// const BASE_URL = 'https://6216b55b71e7672e53694d32.mockapi.io/';
-// // const KEY = '';
-
-// async function fetchWithErrorHandling(url = '') {
-//     const response = await fetch(url);
-//     return response.ok
-//         ? await response.json()
-//         : Promise.reject(new Error('Not found'));
-// }
-
-// function fetchAddcotact(endpoint) {
-//     return fetchWithErrorHandling(`${BASE_URL}/${endpoint}`);
-// }
-
-// const fetches = { fetchAddcotact }
-
-// export default fetches
